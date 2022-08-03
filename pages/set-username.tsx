f299df269ec07d094821bfbb1552e04dfe86ccdd
Ahmet Kilinc <akx9@icloud.com>
2022-08-03 15:17:56 +0100

    protect set-username route

--- pages/set-username.tsx
+++ pages/set-username.tsx
@@ -2,6 +2,9 @@ import axios from "axios";
 import { useRef } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { unstable_getServerSession } from "next-auth";
+import { GetServerSideProps } from "next";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 const Home = () => {
 	const router = useRouter();
@@ -27,6 +30,23 @@ const Home = () => {
 			</form>
 		</div>
 	);
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await unstable_getServerSession(context.req, context.res, authOptions);
+
+	if (!session) {
+		return {
+			props: {},
+		};
+	} else {
+		return {
+			redirect: {
+				destination: "/me",
+				permanent: false,
+			},
+		};
+	}
 };
 
 export default Home;
